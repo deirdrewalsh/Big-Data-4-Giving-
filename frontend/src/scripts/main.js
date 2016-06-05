5cf1fa752610953bcb5049ff433459208bc7281d
Lawrence Coleman <philosopherlawr@gmail.com>
2016-06-05 09:14:50 -0500

    FE working on panel roll over

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -117,4 +117,27 @@
       legend.addTo(simpleMap);
     });
   });
+
+  // external js: isotope.pkgd.js
+
+  var $grid = $('.grid').isotope({
+    itemSelector: '.grid-item',
+    stagger: 30
+  });
+
+  $('.filter-button-group').on( 'click', '.button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+
+
 })();
