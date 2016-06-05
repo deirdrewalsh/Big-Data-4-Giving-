5d4364d667fa726aa2b33cd0e3d162d71bce71a2
JMieding <jtmieding@gmail.com>
2016-06-05 08:15:35 -0500

    Legend change, highlight census tracts

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -33,17 +33,22 @@
     var dataset = 'Low Poverty';
 
     function getColor(d) {
-      return d > 99 ? '#800026' :
-             d > 80  ? '#BD0026' :
-             d > 65  ? '#E31A1C' :
-             d > 50  ? '#FC4E2A' :
-             d > 35   ? '#FD8D3C' :
-             d > 25   ? '#FEB24C' :
-             d > 13   ? '#FED976' :
-                        '#FFEDA0';
+      return d > 99 ? '#FFEDA0' :
+             d > 80 ? '#FED976' :
+             d > 65 ? '#FEB24C' :
+             d > 50 ? '#FD8D3C' :
+             d > 35 ? '#FC4E2A' :
+             d > 25 ? '#E31A1C' :
+             d > 13 ? '#BD0026' :
+                      '#800026';
     }
 
     var url = 'http://127.0.0.1:5000/query?xmin=' + xmin.toString() + '&xmax=' + xmax.toString() + '&ymin=' + ymin.toString() + '&ymax=' + ymax.toString() + '&dataset=' + dataset;
+    if (typeof geojson == 'defined') {
+      simpleMap.removeLayer(geojson);
+      simpleMap.removeLayer(legend);
+      console.log(typeof geojson);
+    }
     $.getJSON(url, function(data) {
       console.log(data);
       if (!data.hasOwnProperty('features')) {
@@ -92,23 +97,24 @@
         };
       }});
 
-      geoJson.addTo(simpleMap);
+      geojson.addTo(simpleMap);
 
       var legend = L.control({position: 'bottomright'});
       legend.onAdd = function(map) {
         var div = L.DomUtil.create('div', 'info legend');
-        var grades = [0, 13, 25, 35, 50, 65, 80, 99];
-        var labels = [];
+        var grades = [99, 80, 65, 50, 35, 25, 13, 0];
+        var labels = ['Least Need', '', '', '', '', '', '', 'Greatest Need'];
 
         // loop through our density intervals and generate a label with a colored square for each interval
         for (var i = 0; i < grades.length; i++) {
           div.innerHTML +=
             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+            labels[i] + '<br>'
         };
 
         return div;
       };
       legend.addTo(simpleMap);
+    });
   });
-})()
+})();
